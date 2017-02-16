bbc39506b117c98e8895b2978a342784ffc10e60
Rémy <remdub@live.fr>
2017-02-16 11:35:11 +0100

    pigeon moving

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-var pigeon = document.createElement('div');
-pigeon.className = 'pigeon';
+function createPigeon() {
+    var pigeon = document.createElement('div');
+    pigeon.className = 'pigeon';
 
-console.log(pigeon);
-pigeon.style.top = Math.floor((Math.random()*87)+1) + '%';
-pigeon.style.left = Math.floor((Math.random()*92)+1) + '%';
+    pigeon.style.top = Math.floor((Math.random()*87)+1) + '%';
+    pigeon.style.left = Math.floor((Math.random()*92)+1) + '%';
 
-console.log(Math.floor((Math.random()*87)+1));
 
-document.getElementById('gameboard').appendChild(pigeon)
+    document.getElementById('gameboard').appendChild(pigeon)
+
+    pigeon.addEventListener("click", function() {
+        this.parentNode.removeChild(this);
+        createPigeon();
+
+    });
+}
+
+function movePigeon() {
+    setTimeout(function() {
+        var pigeons = document.getElementsByClassName('pigeon');
+        for (var i = 0; i < pigeons.length; i++) {
+            pigeons[i].style.top = Math.floor((Math.random()*87)+1) + '%';
+            pigeons[i].style.left = Math.floor((Math.random()*92)+1) + '%';
+        }
+        movePigeon();
+    }, 1000);
+}
+
+createPigeon();
+movePigeon();
