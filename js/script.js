13ae32703550043b7047fd7e70e6f0974cfc4983
bl00m <leon.pierre@openmailbox.org>
2017-02-16 16:53:05 +0100

    onclick on board & BAZOOKA MOTHER FUCKER !

--- js/script.js
+++ js/script.js
@@ -1,34 +1,9 @@
-var score = 0;
-
 function createPigeon() {
     var pigeon = document.createElement('div');
     pigeon.className = 'pigeon';
-
     pigeon.style.top = Math.floor((Math.random()*87)+1) + '%';
     pigeon.style.left = Math.floor((Math.random()*92)+1) + '%';
-
-
     document.getElementById('gameboard').appendChild(pigeon)
-
-    pigeon.addEventListener("click", function() {
-        this.style.transform = 'translateY(40em) rotate(1080deg)';
-        var element = this;
-        updateScore(150);
-        setTimeout(function() {
-            element.parentNode.removeChild(element);
-        }, 1000);
-    });
-}
-
-function movePigeon() {
-    setTimeout(function() {
-        var pigeons = document.getElementsByClassName('pigeon');
-        for (var i = 0; i < pigeons.length; i++) {
-            pigeons[i].style.top = Math.floor((Math.random()*87)+1) + '%';
-            pigeons[i].style.left = Math.floor((Math.random()*92)+1) + '%';
-        }
-        movePigeon();
-    }, 1000);
 }
 
 function morePigeons() {
@@ -38,14 +13,14 @@ function morePigeons() {
             createPigeon();
             morePigeons();
 
-        }, 3000 - score/5);  
+        }, 2000 - score/5);
+        
     } else {
         alert('Game Over');
         document.getElementById('gameboard').innerHTML = '';
         updateScore(0 - score);
         morePigeons();
     }
-
 }
 
 function updateScore(num) {
@@ -53,11 +28,74 @@ function updateScore(num) {
     document.getElementById('score').innerHTML = score;
 }
 
-document.getElementById('gameboard').addEventListener('click', function() {
-    updateScore(-50);
+function launchBazooka() {
+    console.log('BAAAZOOOKA');
+    bazooka = true;
+    comboCount = 0;
+    setTimeout(function () {
+        bazooka = false;
+    }, 10000);
+}
+
+var score = 0;
+var bazooka = false;
+var comboCount = 0;
+
+document.getElementById('gameboard').addEventListener('click', function(event) {
+    if (!bazooka) {
+        var pigeons = document.getElementsByClassName('pigeon');
+
+        var touche = false;
+        for (var i=0; i<pigeons.length; i++) {
+            pigeonPos = pigeons[i].getBoundingClientRect();
+
+            if (event.clientX > pigeonPos.left && event.clientX < pigeonPos.left + 100 && event.clientY > pigeonPos.top && event.clientY < pigeonPos.top + 92) {
+                pigeons[i].className = 'deadpigeon';
+                updateScore(100);
+                if (comboCount > 4) {
+                    launchBazooka();
+                } else {
+                    comboCount++;
+                }
+                touche = true;
+            }
+        }
+
+        if (!touche) {
+            updateScore(-50);
+            comboCount = 0;
+        }
+
+    } else {
+        var pigeons = document.getElementsByClassName('pigeon');
+
+        for (var i=pigeons.length-1; i>=0; i--) {
+            pigeonPos = pigeons[i].getBoundingClientRect();
+
+            if (Math.abs(event.clientX - pigeonPos.left) < 200) {
+                if (Math.abs(event.clientY - pigeonPos.top) < 200) {
+                    pigeons[i].className = 'deadpigeon';
+                    updateScore(100);
+                }
+            }
+        }
+    }
 });
 
+setInterval(function() {
+    var pigeons = document.getElementsByClassName('pigeon');
+    for (var i = 0; i < pigeons.length; i++) {
+        pigeons[i].style.top = Math.floor((Math.random()*87)+1) + '%';
+        pigeons[i].style.left = Math.floor((Math.random()*92)+1) + '%';
+    }
+}, 1000);
+
+setInterval(function() {
+    var pigeons = document.getElementsByClassName('deadpigeon');
+    for (var i = 0; i < pigeons.length; i++) {
+        pigeons[i].parentNode.removeChild(pigeons[i]);
+    }
+}, 10000);
 
 createPigeon();
-movePigeon();
 morePigeons();
